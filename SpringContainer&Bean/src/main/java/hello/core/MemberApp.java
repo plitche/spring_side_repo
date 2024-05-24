8a48d4bce4717e2fdb76df101c7720ff9860248e
plitche <kysu728@naver.com>
2024-05-24 22:54:46 +0900

    스프링으로 전환

--- SpringContainer&Bean/src/main/java/hello/core/MemberApp.java
+++ SpringContainer&Bean/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // Spring 컨테이너 : 모든 객체들 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         // MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
