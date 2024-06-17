f3d3a1b5f8e38243b2e59d9b906ce9b6b842d022
권용수 <yskwon@itembay.com>
2024-06-17 17:12:29 +0900

    test Commit contents with other device and other account

--- NumberBaseball/js/common.js
+++ NumberBaseball/js/common.js
@@ -2,37 +2,41 @@ const regex1 = /^\d{4}$/;
 
 // 랜덤 숫자 만들기
 function makeRandomNum() {
-    const digits = [];
+  const digits = [];
 
-    while (digits.length < 4) {
-        const randomDigit = Math.floor(Math.random() * 10);
-        if (!digits.includes(randomDigit)) {
-            digits.push(randomDigit);
-        }
+  while (digits.length < 4) {
+    const randomDigit = Math.floor(Math.random() * 10);
+    if (!digits.includes(randomDigit)) {
+      digits.push(randomDigit);
     }
+  }
 
-    return digits.join('');
+  return digits.join("");
 }
 
 // 숫자 정규식 체크
 function isNumber(number) {
-     return regex1.test(number);
+  return regex1.test(number);
 }
 
 // 중복 숫자 여부 체크
 function hasDuplicates(number) {
-    let numArray = number.split('');
-    let numSet = new Set(numArray);
-    return numSet.size !== numArray.length;
+  let numArray = number.split("");
+  let numSet = new Set(numArray);
+  return numSet.size !== numArray.length;
 }
 
 // 숫자 예시 제어
 function exampleNumbersControl() {
-    $('.num').on('click', function() {
-        if ($(this).hasClass('hide')) {
-            $(this).removeClass('hide');
-        } else {
-            $(this).addClass('hide');
-        }
-    })
+  $(".num").on("click", function () {
+    if ($(this).hasClass("hide")) {
+      $(this).removeClass("hide");
+    } else {
+      $(this).addClass("hide");
+    }
+  });
+}
+
+function testCommit() {
+  return "test Commit contents with other device and other account";
 }
