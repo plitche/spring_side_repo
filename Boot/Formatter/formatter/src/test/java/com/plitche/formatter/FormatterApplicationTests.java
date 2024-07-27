ad733bb4013365bbbc1a26c4c4d90e1ebb208e54
plitche <kysu728@naver.com>
2024-07-27 16:39:31 +0900

    init prject

---
+++ Boot/Formatter/formatter/src/test/java/com/plitche/formatter/FormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plitche.formatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
