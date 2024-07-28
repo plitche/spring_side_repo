511ecd4753b506575bc88b3cac5b85b6754388ab
plitche <kysu728@naver.com>
2024-07-28 21:41:29 +0900

    code formatter 설정

---
+++ Boot/Formatter/src/main/java/com/plitche/formatter/FormatterApplication.java
@@ -0,0 +1,33 @@
+package com.plitche.formatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatterApplication {
+
+    // ./gradlew spotlessCheck
+    // ./gradlew spotlessApply
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormatterApplication.class, args);
+
+        int[] intArray = new int[5];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+        intArray[3] = 4;
+        intArray[4] = 5;
+
+        List<String> stringArrayList = new ArrayList<>();
+        stringArrayList.stream()
+                .filter(x -> x.equals("1"))
+                .map(x -> x.equals("1"))
+                .collect(Collectors.toList());
+        stringArrayList.stream().forEach(System.out::println);
+    }
+}
