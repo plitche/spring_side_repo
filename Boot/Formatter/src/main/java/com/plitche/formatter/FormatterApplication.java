53b3f712c3c78b3badf7103b2398bc4ac8592db2
권용수 <kysu728@naver.com>
2024-08-06 16:11:51 +0900

    코드 컨벤션 formatter 소스 코드 정리

--- Boot/Formatter/src/main/java/com/plitche/formatter/FormatterApplication.java
+++ Boot/Formatter/src/main/java/com/plitche/formatter/FormatterApplication.java
@@ -7,9 +7,6 @@ package com.plitche.formatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.plitche.apply.example.Library;
-import com.plitche.apply.utils.Utils;
-
 @SpringBootApplication
 public class FormatterApplication {
 
@@ -20,10 +17,5 @@ public class FormatterApplication {
     SpringApplication.run(FormatterApplication.class, args);
 
     System.out.println("Hello from MainApp!");
-
-    Library lib = new Library();
-    lib.printMessage();
-
-    System.out.println(Utils.getGreeting());
   }
 }
--- Boot/Formatter/src/main/java/com/plitche/notApply/service/impl/ProductServiceImpl.java
+++ Boot/Formatter/src/main/java/com/plitche/notApply/service/impl/ProductServiceImpl.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Collections;
+import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service("productServiceImpl")
